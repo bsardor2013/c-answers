49894dec03317166cb9cc2041ce3e5cae3044c4b
Sardor <salohiddin8609@gmail.com>
2025-11-27 03:31:48 +0000

    integer tugadi

---
+++ integer/26.py
@@ -0,0 +1,3 @@
+a={0:"yakshanba",1:"dushshanba",2:"sheshanba",3:"chorshanba",4:"payshanba",5:"juma",6:"shanba"}
+k=int(input())
+print(a[k%7+2])
---
+++ integer/29.py
@@ -0,0 +1,4 @@
+a,b,c=map(int,input().split(","))
+d,f=a//c,b//c
+g,h=a%c,b%c
+print(d*f,g*h)
